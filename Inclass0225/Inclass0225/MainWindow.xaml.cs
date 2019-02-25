978248a8ff8c242668da23b8b0943e7a16a87c21
Ruth Wen <ruth.wen@ou.edu>
2019-02-25 11:51:06 -0600

    json0225

---
+++ Inclass0225/Inclass0225/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inclass0225
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                //https://got-quotes.herokuapp.com/quotes
+                var response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    HerokuQuote q = JsonConvert.DeserializeObject<HerokuQuote>(content);
+
+                }
+            }
+        }
+    }
+}
